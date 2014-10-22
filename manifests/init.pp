f49d3f6bcb7250e8722810a67b0afc22cb7a56db
Edmond Negado <enegado@rockstar.ucsd.edu>
2014-10-22 14:09:49 -0700

    added ensure for removeDenyHosts.erb

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,18 @@
 
 class deny-hosts inherits denyhosts
 {
+
+
+  file { 'removedenyhosts':
+    ensure => present,
+    path    => '/usr/local/bin/removeDenyHosts',
+    mode    => 0700,
+    owner   => 'root',
+    group   => 'root',
+    content => template('deny-hosts/removeDenyHosts.erb'),
+  }
+
+
   
 }
 
