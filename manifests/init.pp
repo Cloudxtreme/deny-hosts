e29a75e7fbba354917edae29d33aaf2955c8500c
Edmond Negado <enegado@rockstar.ucsd.edu>
2014-10-28 11:40:15 -0700

    removed the script and put in devops and took out its file resource

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,16 +2,6 @@
 class deny-hosts inherits denyhosts
 {
 
-   # this can be moved to the devops class
-  file { 'removedenyhosts':
-    ensure => present,
-    path    => '/usr/local/bin/removeDenyHosts',
-    mode    => 0700,
-    owner   => 'root',
-    group   => 'root',
-    content => template('deny-hosts/removeDenyHosts.erb'),
-  }
-
   ## temp fix until puppetlabs/denyhosts fixes the option patch-01
   file { '/etc/denyhosts/denyhosts.cfg':
     owner   => root,
@@ -20,8 +10,6 @@ class deny-hosts inherits denyhosts
     content => template('denyhosts/denyhosts.conf.erb'),
     notify  => Service['denyhosts'],
   }
-
-
   
 }
 
