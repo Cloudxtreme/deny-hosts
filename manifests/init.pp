eac5d2c8bbd9aab5c8b30fe19c056e79874f53d6
Edmond Negado <enegado@rockstar.ucsd.edu>
2014-10-28 11:18:38 -0700

    added the proper path to the config temperarily

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 class deny-hosts inherits denyhosts
 {
 
-
+   # this can be moved to the devops class
   file { 'removedenyhosts':
     ensure => present,
     path    => '/usr/local/bin/removeDenyHosts',
@@ -11,6 +11,17 @@ class deny-hosts inherits denyhosts
     group   => 'root',
     content => template('deny-hosts/removeDenyHosts.erb'),
   }
+
+  ## temp fix until puppetlabs/denyhosts fixes the option patch-01
+  file { '/etc/denyhosts/denyhosts.cfg':
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('denyhosts/denyhosts.conf.erb'),
+    notify  => Service['denyhosts'],
+  }
+
+
   
 }
 
